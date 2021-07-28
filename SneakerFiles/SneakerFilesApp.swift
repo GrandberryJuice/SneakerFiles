f1f6487b810cabbbd6be0795303a72ae4a0673f6
Kenny <krgrndbr@gmail.com>
2021-07-28 13:35:35 -0400

    Initial Commit

---
+++ SneakerFiles/SneakerFilesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SneakerFilesApp.swift
+//  SneakerFiles
+//
+//  Created by Kgrandbe on 7/28/21.
+//
+
+import SwiftUI
+
+@main
+struct SneakerFilesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
